0b3c8eb1843c5df5f5c8c516e19fde0ac2aa6a0f
TheKingOfLettuce <68159617+TheKingOfLettuce@users.noreply.github.com>
2025-07-01 14:34:50 -0400

    comments

--- Lettuce.Collections/BiDirectionalDictionary.cs
+++ Lettuce.Collections/BiDirectionalDictionary.cs
@@ -2,17 +2,22 @@ namespace Lettuce.Collections;
 
 /// <summary>
 /// A two way dictionary that maps keys to values and values to keys. This collection <br/>
-/// is backed by two <see cref="Dictionary{Key, Value}"/> collections
+/// is backed by two <see cref="Dictionary{TKey, TValue}"/> collections
 /// </summary>
-/// <typeparam name="Key">the key type to map both ways</typeparam>
-/// <typeparam name="Value">the value type to map both ways</typeparam>
-public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value : notnull {
-    private readonly Dictionary<Key, Value> _keyValuePairs;
-    private readonly Dictionary<Value, Key> _valueKeyPairs;
+/// <typeparam name="TKey">the key type to map both ways</typeparam>
+/// <typeparam name="TValue">the value type to map both ways</typeparam>
+public class BiDirectionalDictionary<TKey, TValue> where TKey : notnull where TValue : notnull {
+    private readonly Dictionary<TKey, TValue> _keyValuePairs;
+    private readonly Dictionary<TValue, TKey> _valueKeyPairs;
 
+    /// <summary>
+    /// Initializes the two backing <see cref="Dictionary{Key, TValue}"/> by calling the default constructor
+    /// </summary>
+    /// <seealso cref="Dictionary{TKey, TValue}()"/>
+    /// <seealso cref="Dictionary{TValue, TKey}()"/>
     public BiDirectionalDictionary() {
-        _keyValuePairs = new Dictionary<Key, Value>();
-        _valueKeyPairs = new Dictionary<Value, Key>();
+        _keyValuePairs = new Dictionary<TKey, TValue>();
+        _valueKeyPairs = new Dictionary<TValue, TKey>();
     }
 
     /// <summary>
@@ -21,7 +26,7 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     /// <param name="key">the key to check association with</param>
     /// <returns>the value associated to that key</returns>
     /// <exception cref="KeyNotFoundException">thrown if key is not found in the dictionary</exception>
-    public Value GetValue(Key key) {
+    public TValue GetValue(TKey key) {
         if (!ContainsKey(key)) {
             throw new KeyNotFoundException($"Key {key} does not exist in the map");
         }
@@ -34,8 +39,8 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     /// </summary>
     /// <param name="key">the key to check</param>
     /// <param name="value">when this method returns, this will be the associated value of the key, or default if key not found</param>
-    /// <returns><see cref="true"/> if the dictionary contained key, <see cref="false"/> otherwise</returns>
-    public bool TryGetValue(Key key, out Value? value) {
+    /// <returns><c>true</c> if the dictionary contained key, <c>false</c> otherwise</returns>
+    public bool TryGetValue(TKey key, out TValue? value) {
         try {
             value = GetValue(key);
             return true;
@@ -52,7 +57,7 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     /// <param name="value">the value to check association with</param>
     /// <returns>the key associated to that value</returns>
     /// <exception cref="KeyNotFoundException">thrown if value is not found in the dictionary</exception>
-    public Key GetKey(Value value) {
+    public TKey GetKey(TValue value) {
         if (!ContainsValue(value)) {
             throw new KeyNotFoundException($"Value {value} does not exist in the map");
         }
@@ -64,9 +69,9 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     /// Attempts to fetch the key associated with the value
     /// </summary>
     /// <param name="value">the value to check</param>
-    /// <param name="Key">when this method returns, this will be the associated key of the value, or default if value not found</param>
-    /// <returns><see cref="true"/> if the dictionary contained value, <see cref="false"/> otherwise</returns>
-    public bool TryGetKey(Value value, out Key? key) {
+    /// <param name="key">when this method returns, this will be the associated key of the value, or default if value not found</param>
+    /// <returns><c>true</c> if the dictionary contained value, <c>false</c> otherwise</returns>
+    public bool TryGetKey(TValue value, out TKey? key) {
         try {
             key = GetKey(value);
             return true;
@@ -78,13 +83,13 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     }
 
     /// <summary>
-    /// Adds the pair of key <-> value to bi-directional associate
+    /// Adds the pair of key, value to bi-directional associate
     /// </summary>
     /// <param name="key">the key to add</param>
     /// <param name="value">the value to add</param>
     /// <exception cref="ArgumentException">throws if the <paramref name="key"/> or <paramref name="value"/> is already in the dictionary</exception>
     /// <exception cref="ArgumentNullException">throws if the <paramref name="key"/> or <paramref name="value"/> is null</exception>
-    public void Add(Key key, Value value) {
+    public void Add(TKey key, TValue value) {
         if (ContainsKey(key)) {
             throw new ArgumentException("Key already exists in pair", nameof(key));
         }
@@ -98,14 +103,15 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
 
     /// <summary>
     /// Attempts to remove the bi-directional association via the key
+    /// </summary>
     /// <param name="key">the key to remove in the association</param>
     /// <returns>the value in the association, or null if the key was not found in the dictionary</returns>
-    public Value? RemoveByKey(Key key) {
+    public TValue? RemoveByKey(TKey key) {
         if (!ContainsKey(key)) {
             return default;
         }
 
-        Value toRemove = _keyValuePairs[key];
+        TValue toRemove = _keyValuePairs[key];
         _ = _keyValuePairs.Remove(key);
         _ = _valueKeyPairs.Remove(toRemove);
 
@@ -114,14 +120,15 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
 
     /// <summary>
     /// Attempts to remove the bi-directional association via the value
+    /// </summary>
     /// <param name="value">the value to remove in the association</param>
     /// <returns>the key in the association, or null if the value was not found in the dictionary</returns>
-    public Key? RemoveByValue(Value value) {
+    public TKey? RemoveByValue(TValue value) {
         if (!ContainsValue(value)) {
             return default;
         }
 
-        Key toRemove = _valueKeyPairs[value];
+        TKey toRemove = _valueKeyPairs[value];
         _ = _valueKeyPairs.Remove(value);
         _ = _keyValuePairs.Remove(toRemove);
 
@@ -132,8 +139,8 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     /// Checks to see if the key exists in the bi-directional dictionary
     /// </summary>
     /// <param name="key">the key to check</param>
-    /// <returns><see cref="true"/> if the key exists, <see cref="false"/> otherwise</returns>
-    public bool ContainsKey(Key key) {
+    /// <returns><c>true</c> if the key exists, <c>false</c> otherwise</returns>
+    public bool ContainsKey(TKey key) {
         return _keyValuePairs.ContainsKey(key);
     }
 
@@ -141,8 +148,8 @@ public class BiDirectionalDictionary<Key, Value> where Key : notnull where Value
     /// Checks to see if the value exists in the bi-directional dictionary
     /// </summary>
     /// <param name="value">the value to check</param>
-    /// <returns><see cref="true"/> if the value exists, <see cref="false"/> otherwise</returns>
-    public bool ContainsValue(Value value) {
+    /// <returns><c>true</c> if the value exists, <c>false</c> otherwise</returns>
+    public bool ContainsValue(TValue value) {
         return _valueKeyPairs.ContainsKey(value);
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Lettuce.Collections
 
+Various collections and data structures for your Lettuce needs
+
+## BiDirectionalDictionary
+
+A dictionary that contains two way associations between the keys and values. That is, you can use the dictionary like normal to pass in a key and get the value out, but you can also pass in the value to get the key out
+
+### Examples
+
+```csharp
+BiDirectionalDictionary<string, string> nickNames = new BiDirectionalDictionary<string, string>();
+nickNames.Add("Cameron", "Cam");
+nickNames.Add("Tommy", "Tom");
+
+nickNames.GetValue("Cameron"); // returns "Cam"
+nickNames.GetKey("Cam"); // returns "Cameron"
+```
